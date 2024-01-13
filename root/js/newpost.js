8dfe61fd6e10768b0c7b61ffeec04afec4f87789
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-13 16:44:24 +0100

    references fix

--- root/js/newpost.js
+++ root/js/newpost.js
@@ -1,2 +1,16 @@
-
+function createPost() {
+    var form = document.getElementById('postForm');
+    var formData = new FormData(form);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'save_post.php', true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        alert('Post creato con successo!');
+      } else {
+        alert('Errore durante la creazione del post.');
+      }
+    };
+    xhr.send(formData);
+  }
   
