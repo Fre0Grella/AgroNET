1d340037fac703460349127b5fbf01480b822751
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-14 16:10:14 +0100

    removed a function in spawn_users.sql

--- db/popola_utenti.sql
+++ db/popola_utenti.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (username, email, password_hash, created_at)
+INSERT INTO users (username, email, password, created_at)
 SELECT
     CONCAT('utente', UUID()) AS username,
     CONCAT('utente', UUID(), '@example.com') AS email,
-    bcrypt('password123', 10) AS password_hash,
+    "hashedpassword" AS password,
     CURRENT_TIMESTAMP AS created_at
 FROM
     (SELECT (ROW_NUMBER() OVER (ORDER BY RAND())) AS id FROM information_schema.tables LIMIT 50) AS user_numbers;
