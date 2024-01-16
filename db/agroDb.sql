88b2dea6ec7c48513e164710690eae03a2826de5
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-16 15:32:45 +0100

    new columns in users table profile_image LONGBLOB

--- db/agroDb.sql
+++ db/agroDb.sql
@@ -15,7 +15,8 @@ CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    profile_image LONGBLOB
 );
 
 CREATE TABLE posts (
