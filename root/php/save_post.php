5771574dfd52fbb9584f1f3cf91694e2a9e12a07
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-17 16:25:51 +0100

    fixed bug in save_post.php

--- root/php/save_post.php
+++ root/php/save_post.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "../bootstrap.php";
 
 
 $description = $_POST['description'];
+$userId = $_SESSION['user_id'];
 
 if ($_FILES['image']['size'] > 0) {
     $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
@@ -16,9 +17,9 @@ $category = $_POST['category'];
 
 
 if ($category == 'true') {
-    $sql = "INSERT INTO posts (description, image_data) VALUES ('$description', '$image')";
+    $sql = "INSERT INTO posts (user_id, description, image_data) VALUES ('$userId', '$description', '$image')";
 } else {
-    $sql = "INSERT INTO posts (description, category, image_data) VALUES ('$description', 0, '$image')";
+    $sql = "INSERT INTO posts (user_id, description, category, image_data) VALUES ('$userId','$description', 0, '$image')";
 }
 
 
