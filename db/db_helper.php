4b6e6733f713806a78004ee605aac22b762d3f9b
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-17 15:09:09 +0100

    new function in DB_helper.php, postFromFollower

--- db/db_helper.php
+++ db/db_helper.php
@@ -18,6 +18,22 @@ class DatabaseHelper{
         $this->db->close();
     }
 
+    function postFromFollowed($user_id) {
+        $stmt = this->db->prepare("SELECT p.post_id, p.user_id, p.description, p.category, p.image_data, p.created_at, p.likes
+        FROM posts p
+        JOIN followers f ON p.user_id = f.followed_id
+        WHERE f.follower_id = ?
+        ORDER BY p.likes DESC
+        LIMIT 50;
+        ");
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+        
+    }
+
 }
 
 ?>
