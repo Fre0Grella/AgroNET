f1065b17240cfc1a1672389fdf0d95ec466807b0
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2024-01-17 17:43:14 +0100

    login js

--- root/js/formPassword.js
+++ root/js/formPassword.js
@@ -6,3 +6,19 @@ togglePassword.addEventListener('click', () => {
         password.setAttribute('type', type);
         document.getElementById("togglePassword").classList.toggle('bi-eye');
 });
+
+function loginCheck() {
+        var form = document.getElementById('button-login');
+        var formData = new FormData(form);
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'php/login.php', true);
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            alert('Post creato con successo!');
+          } else {
+            alert('Errore durante la creazione del post. Codice HTTP:' + xhr.status );
+          }
+        };
+        xhr.send(formData);
+      }
