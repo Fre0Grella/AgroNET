a8379a656c925f9382196f712ccd769283a10e2e
Marco Galeri <marco.galeri@studio.unibo.it>
2024-01-17 14:54:24 +0100

    try add some query

--- db/db_helper.php
+++ db/db_helper.php
@@ -18,6 +18,35 @@ class DatabaseHelper{
         $this->db->close();
     }
 
+    public function getRandomPosts(){
+        $stmt = $this->db->prepare("SELECT u.username, u.profile_image, p.image_data FROM users AS u RIGHT JOIN posts AS p ON u.user_id = p.user_id JOIN followers as f ON f.follower_id = p.user_id WHERE followed_id = ? ORDER BY created_at DESC LIMIT 50");
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getGreenPosts() {
+        $stmt = $this->db->prepare("SELECT u.username, u.profile_image, p.image_data FROM users AS u RIGHT JOIN posts AS p ON u.user_id = p.user_id JOIN followers as f ON f.follower_id = p.user_id WHERE followed_id = ? AND category = true ORDER BY created_at DESC LIMIT 50");
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getTractorPosts() {
+        $stmt = $this->db->prepare("SELECT u.username, u.profile_image, p.image_data FROM users AS u RIGHT JOIN posts AS p ON u.user_id = p.user_id JOIN followers as f ON f.follower_id = p.user_id WHERE followed_id = ? AND category = false ORDER BY created_at DESC LIMIT 50");
+        $stmt->bind_param('i', $user_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    
+
 }
 
 ?>
