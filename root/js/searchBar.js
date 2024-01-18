6b16d432640fb7c9b7a77f79695e906f4f163acd
Marco Galeri <marco.galeri@studio.unibo.it>
2024-01-18 12:21:09 +0100

    fix search bar resize

--- root/js/searchBar.js
+++ root/js/searchBar.js
@@ -1,51 +1,54 @@
 $(document).ready(function() {
+    console.log(window.innerWidth);
+    if(window.outerWidth > 600) {
+        
+        var searchBar = $(".searchBar");
+        var scrollPrev = 0
 
-    var searchBar = $(".searchBar");
-    var scrollPrev = 0
+        $(window).scroll(function() {
+            var scrolled = $(window).scrollTop();
+            var firstScrollUp = false;
+            var firstScrollDown = false;
 
-    $(window).scroll(function() {
-        var scrolled = $(window).scrollTop();
-        var firstScrollUp = false;
-        var firstScrollDown = false;
-
-        if ( scrolled > 0 ) {
-            if ( scrolled > scrollPrev ) {
-                firstScrollUp = false;
-                if ( scrolled < searchBar.height() + searchBar.offset().top ) {
-                    if ( firstScrollDown === false ) {
-                        var topPosition = searchBar.offset().top;
+            if ( scrolled > 0 ) {
+                if ( scrolled > scrollPrev ) {
+                    firstScrollUp = false;
+                    if ( scrolled < searchBar.height() + searchBar.offset().top - parseInt(searchBar.css('margin-top'), 10) ) {
+                        if ( firstScrollDown === false ) {
+                            var topPosition = searchBar.offset().top - parseInt(searchBar.css('margin-top'), 10);
+                            searchBar.css({
+                                "top": topPosition + "px"
+                            });
+                            firstScrollDown = true;
+                        }
                         searchBar.css({
-                            "top": topPosition + "px"
+                            "position": "absolute"
                         });
-                        firstScrollDown = true;
-                    }
-                    searchBar.css({
-                        "position": "absolute"
-                    });
-                } else {
-                    searchBar.css({
-                        "position": "fixed",
-                        "top": "-" + searchBar.height() + "px"
-                    });
-                }
-            } else {
-                firstScrollDown = false;
-                if ( scrolled > searchBar.offset().top ) {
-                    if ( firstScrollUp === false ) {
-                        var topPosition = searchBar.offset().top;
+                    } else {
                         searchBar.css({
-                            "top": topPosition + "px"
+                            "position": "fixed",
+                            "top": "-" + searchBar.height() + "px"
                         });
-                        firstScrollUp = true;
                     }
-                    searchBar.css({
-                        "position": "absolute"
-                    });
                 } else {
-                    searchBar.removeAttr("style");
+                    firstScrollDown = false;
+                    if ( scrolled > searchBar.offset().top - parseInt(searchBar.css('margin-top'), 10)) {
+                        if ( firstScrollUp === false ) {
+                            var topPosition = searchBar.offset().top - parseInt(searchBar.css('margin-top'), 10);
+                            searchBar.css({
+                                "top": topPosition + "px"
+                            });
+                            firstScrollUp = true;
+                        }
+                        searchBar.css({
+                            "position": "absolute"
+                        });
+                    } else {
+                        searchBar.removeAttr("style");
+                    }
                 }
+                scrollPrev = scrolled;
             }
-            scrollPrev = scrolled;
-        }
-    });
+        });
+    }
 });
