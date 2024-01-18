f0a318c3a9cb1e940a03917e68446a91109dc20e
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-18 14:56:27 +0100

    refactor in home.php

--- root/home.php
+++ root/home.php
@@ -1,10 +1,12 @@
 <?php
 require_once("bootstrap.php");
 
+$user_id = $_SESSION["user_id"];
+
 $templateParams["title"] = "AgroNET - Home";
-$templateParams["greenPost"] = $dbh->getGreenPosts();
-$templateParams["tractorPost"] = $dbh->getTractorPosts();
-$templateParams["allPost"] = $dbh->getAllPosts();
+$templateParams["greenPost"] = $dbh->postFromFollowed($user_id);
+$templateParams["tractorPost"] = $dbh->postTractorFromFollowed($user_id);
+$templateParams["allPost"] = $dbh->postFromFollowed($user_id);
 
 
 require("template/home.php");
