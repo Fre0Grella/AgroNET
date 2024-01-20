0465d72ea839c38ccfac5535ba4f312dd568dee9
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-20 16:47:20 +0100

    completed doc in db_writer.php

--- db/db_writer.php
+++ db/db_writer.php
@@ -4,9 +4,9 @@ require_once __DIR__ . "/db_reader.php";
 class DataBaseWriter extends DataBaseReader{
 
     /**
-     * @param $email
-     * @param $password
-     * @param $username
+     * @param $email , input email
+     * @param $password , input password to check if exist
+     * @param $username , input username to check if exist
      * @return void
      */
     public function registerUser($email, $password, $username): void
@@ -18,8 +18,8 @@ class DataBaseWriter extends DataBaseReader{
     }
 
     /**
-     * @param $username
-     * @param $username2
+     * @param $username , username of who create the chat
+     * @param $username2 , username second member of the chat
      * @return void
      */
     public function createChat($username, $username2): void
@@ -31,9 +31,9 @@ class DataBaseWriter extends DataBaseReader{
     }
 
     /**
-     * @param $chat_id
-     * @param $user_id
-     * @param $message
+     * @param $chat_id , id of the chat
+     * @param $user_id , sender of the message
+     * @param $message , string to save in message_text
      * @return void
      */
     public function createMessage($chat_id, $user_id, $message): void
@@ -45,26 +45,26 @@ class DataBaseWriter extends DataBaseReader{
     }
 
     /**
-     * @param $user_id
-     * @param $post_id
+     * @param $user_id , id of the users who liked the post
+     * @param $post_id , id of the liked post
      * @return void
      */
     public function newLike($user_id, $post_id): void
     {
-        $stmt = $this->db->prepare("INSERT INTO agro_db.post_likes (user_id, post_id) VALUES (?, ?);");
+        $stmt = $this->db->prepare("INSERT INTO post_likes (user_id, post_id) VALUES (?, ?);");
         $stmt->bind_Param("ii", $user_id, $post_id);
         $stmt->execute();
         $stmt->close();
     }
 
     /**
-     * @param $user_id
-     * @param $post_id
+     * @param $user_id , id of the users who removed the like
+     * @param $post_id , id of the post where user removed the like
      * @return void
      */
     public function removeLike($user_id, $post_id): void
     {
-        $stmt = $this->db->prepare("DELETE FROM agro_db.post_likes WHERE user_id = ? AND post_id = ?;");
+        $stmt = $this->db->prepare("DELETE FROM post_likes WHERE user_id = ? AND post_id = ?;");
         $stmt->bind_Param("ii", $user_id, $post_id);
         $stmt->execute();
         $stmt->close();
--- root/bootstrap.php
+++ root/bootstrap.php
@@ -2,4 +2,5 @@
 session_start();
 require_once __DIR__ . "/../db/db_helper.php";
 $dbh = new DatabaseHelper("localhost", "root", "", "agro_db", 3306);
+
 ?>
