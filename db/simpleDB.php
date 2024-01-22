52db3f639b73fd0c48e533cfa02422214f05e087
Aldo Visconti <aldovisco20000@gmail.com>
2024-01-22 16:43:52 +0100

    refactor db/simpleDB.php

--- db/simpleDB.php
+++ db/simpleDB.php
@@ -14,7 +14,7 @@ class SimpleDB {
     public function __construct($servername, $username, $password, $dbname, $port) {
         $this->db = new mysqli($servername, $username, $password, $dbname, $port);
         if ($this->db->connect_error) {
-            die("Connection failed: " . $db->connect_error);
+            die("Connection failed: " . $this->db->connect_error);
         }
     }
 
