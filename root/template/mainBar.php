c04edb93494106ec96369e2aed6193bc8142c1e5
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2024-01-31 16:58:11 +0100

    adjust profile and newpost format

--- root/template/mainBar.php
+++ root/template/mainBar.php
@@ -1,7 +1,7 @@
 <nav id="botBar">
     <a href="home.php"><img src="img/icon/home.svg" alt="Home" draggable="false" class="img-fluid p-2 change"></a>
     <a href="search.php"><img src="img/icon/search.svg" alt="Search" draggable="false" class="img-fluid p-2 change"></a>
-    <a href="newpost.html"><img src="img/icon/addPhoto.svg" alt="Add Photo" draggable="false" class="img-fluid p-2 change"></a>
-    <a href="profileSettings.html"><img src="img/icon/settings.svg" alt="Settings" draggable="false" class="img-fluid p-2 change"></a>
+    <a href="newpost.php"><img src="img/icon/addPhoto.svg" alt="Add Photo" draggable="false" class="img-fluid p-2 change"></a>
+    <a href="profileSettings.php"><img src="img/icon/settings.svg" alt="Settings" draggable="false" class="img-fluid p-2 change"></a>
     <a href="profile.php"><img src="img/icon/profile.svg" alt="Profile" draggable="false" class="img-fluid p-2 change"></a>
 </nav>
